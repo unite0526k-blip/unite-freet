c87b96892c17858297a5442ade0bababc8ef7e42
吉田健人 <yoshidataketo@yoshidataketonoMacBook-puro.local>
2026-08-24 15:37:04 +0900

    Use AppShell in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import Sidebar from "./components/Sidebar";
+import AppShell from "./components/AppShell";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,15 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="flex min-h-screen">
-
-          <Sidebar />
-
-          <main className="flex-1 bg-gray-100 p-6">
-            {children}
-          </main>
-
-        </div>
+        <AppShell>{children}</AppShell>
       </body>
     </html>
   );
